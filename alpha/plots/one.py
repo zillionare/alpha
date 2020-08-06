63e5c1c8c726c92cf90d5912b742c83e4dcbcce8
jieyu-tech <code@jieyu.ai>
2020-08-06 22:28:37 +0800

    add plot one

---
+++ alpha/plots/one.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Author: Aaron-Yang [code@jieyu.ai]
+Contributors: 
+
+"""
+import logging
+
+import arrow
+import jqdatasdk as jq
+from pandas import DataFrame
+import numpy as np
+
+from alpha.core.signal import moving_average, polyfit
+
+logger = logging.getLogger(__name__)
+
+class One:
+    def __init__(self):
+        jq.auth('18694978299', '8Bu8tcDpEAHJRn')
+        self.secs = jq.get_all_securities()
+
+    def all_stocks(self):
+        return self.secs.index
+
+    def get_bars(self, code, count, frame, end_dt):
+        if not (code.upper().endswith('.XSHE') or code.upper().endswith('.XSHG')):
+            if code.startswith('60'):
+                code += '.XSHG'
+            else:
+                code += '.XSHE'
+        fields = ['date', 'open', 'high', 'low', 'close', 'volume', 'money', 'factor']
+        return jq.get_bars(code, count, frame, fields, include_now=True, end_dt=end_dt,
+                           fq_ref_date=end_dt)
+
+    def name_of(self,code:str):
+        return self.secs[self.secs.index == code]['display_name'].iat[0]
+
+    def check_buy_limit(self,c1: float, c: float, display_name: str = None):
+        """
+        股价是否达到涨停价
+        :param display_name:
+        :param c1: 前一日收盘价
+        :param c: 当日收盘价
+        :return:
+        """
+        if display_name and display_name.find("ST") != -1:
+            limit = 0.05
+        else:
+            limit = 0.1
+
+        if not all([c, c1]):
+            return False
+
+        return (c + 0.01) / c1 - 1 > limit
+    def screen(self,frame, end_dt=None, adv_lim=25, win=7, a5=0.02, a10=0.001):
+        all = []
+        fired = []
+        if end_dt is None:
+            end_dt = arrow.now().datetime
+
+        for i, code in enumerate(self.all_stocks()):
+            try:
+                name = self.name_of(code)
+                if name.endswith("退"):
+                    continue
+                if name.find("ST") != -1:
+                    continue
+
+                bars = self.get_bars(code, 30, frame, end_dt=end_dt)
+                if len(bars) == 0:
+                    print("get 0 bars", code)
+                    continue
+
+                if arrow.get(bars['date'].iat[-1]).date() != arrow.get(end_dt).date():
+                    continue
+
+                # 30日涨幅必须小于adv_lim
+                if bars['close'].iat[-1] / bars['close'].min() >= 1 + adv_lim / 100:
+                    print(f"{code}涨幅大于", adv_lim)
+                    continue
+
+                ma5 = np.array(moving_average(bars['close'], 5))
+                ma10 = np.array(moving_average(bars['close'], 10))
+
+                err5, coef5, vertex5 = polyfit(ma5[-win:])
+                err10, coef10, vertex10 = polyfit(ma10[-win:])
+
+                vx5, _ = vertex5
+                vx10, _ = vertex10
+                _a5 = coef5[0]
+                _a10 = coef10[0]
+                all.append([code, _a5, _a10, vx5, vx10, err5, err10])
+
+                # print(code, round_list([err5, vx, pred_up, y5, ma5[-1], y10, ma10[-1]],3))
+                # 如果曲线拟合较好，次日能上涨up%以上，10日线也向上，最低点在win/2以内
+                t1 = err5 <= 0.003 and err10 <=0.003
+                t2 = _a5 > a5 and _a10 > a10
+                t3 = (win - 1 > vx5 >= win/2-1) and (vx10 < win/2 - 1)
+                if t1 and t2 and t3:
+                    c1, c0 = bars['close'].iat[-2], bars['close'].iat[-1]
+                    if self.check_buy_limit(c1, c0, name):  # 跳过涨停的
+                        continue
+
+                    print(f"{self.name_of(code)} {code}",[_a5,_a10,vx5,vx10,err5,err10])
+                    fired.append([code, _a5, _a10, vx5, vx10, err5, err10])
+            except Exception as e:
+                print(i, e)
+                continue
+        return DataFrame(data=all,
+                         columns=['code', 'a5', 'a10', 'vx5', 'vx10', 'err_5',
+                                  'err_10'])
--- tests/test_alpha.py
+++ tests/test_alpha.py
@@ -4,8 +4,13 @@
 
 
 import unittest
+
+import arrow
 import jqdatasdk as jq
+from pandas import DataFrame
+
 from alpha import alpha
+import numpy as np
 from alpha.core.signal import moving_average, polyfit
 
 
@@ -15,7 +20,41 @@ class TestAlpha(unittest.TestCase):
     def setUp(self):
         """Set up test fixtures, if any."""
         jq.auth('18694978299', '8Bu8tcDpEAHJRn')
+        self.secs = jq.get_all_securities()
 
+    def all_stocks(self):
+        return self.secs.index
+
+    def get_bars(self, code, count, frame, end_dt):
+        if not (code.upper().endswith('.XSHE') or code.upper().endswith('.XSHG')):
+            if code.startswith('60'):
+                code += '.XSHG'
+            else:
+                code += '.XSHE'
+        fields = ['date', 'open', 'high', 'low', 'close', 'volume', 'money', 'factor']
+        return jq.get_bars(code, count, frame, fields, include_now=True, end_dt=end_dt,
+                           fq_ref_date=end_dt)
+
+    def name_of(self,code:str):
+        return self.secs[self.secs.index == code]['display_name'].iat[0]
+
+    def check_buy_limit(self,c1: float, c: float, display_name: str = None):
+        """
+        股价是否达到涨停价
+        :param display_name:
+        :param c1: 前一日收盘价
+        :param c: 当日收盘价
+        :return:
+        """
+        if display_name and display_name.find("ST") != -1:
+            limit = 0.05
+        else:
+            limit = 0.1
+
+        if not all([c, c1]):
+            return False
+
+        return (c + 0.01) / c1 - 1 > limit
     def tearDown(self):
         """Tear down test fixtures, if any."""
 
@@ -63,3 +102,8 @@ class TestAlpha(unittest.TestCase):
                   f"(x,y):{axis_x:.1f}"
                   f",{axis_y:.2f}")
 
+
+
+    def test_screen(self):
+        results = self.screen('1d', end_dt='2020-08-05 15:00:00')
+        print(results)
