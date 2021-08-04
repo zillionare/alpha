fb39c9f98f6d37882662380fb9abcdc175c2d967
zillionare <code@jieyu.ai>
2021-08-05 01:56:48 +0800

    ✨ feat(): 增加数值排列编码方案pos_encode

---
+++ tests/strategies/test_x.py
@@ -0,0 +1,20 @@
+from alpha.core.features import pos_encode
+import itertools
+import unittest
+import numpy as np
+
+class TextX(unittest.TestCase):
+    def test_position_encoding(self):
+        stationary = np.array([5, 10, 20])
+        results = []
+        for composition in itertools.permutations(stationary):
+            # (5, 10, 30, 20)
+            results.append({
+                "val": pos_encode(stationary, composition),
+                "comp": composition
+            })
+
+        sorted_results = sorted(results, key=lambda x: x["val"])
+        for r in sorted_results:
+            print(f'{round(r["val"], 2)}:{r["comp"]}')
+            
