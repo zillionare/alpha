3e1aa0ff7bffb223fa0bc863d7d291f27c2cf2d3
zillionare <code@jieyu.ai>
2021-09-11 17:22:43 +0800

    ✨ feat: add frame as xticks and fix several bugs
    cacl rect width according to canvas size, remove xtick of candlestick

--- alpha/plotting/candlestick.py
+++ alpha/plotting/candlestick.py
@@ -1,34 +1,34 @@
 # noinspection PyUnresolvedReferences
-import logging
+
 from io import BytesIO
 from typing import Optional, Tuple
-
-import matplotlib
 
 # Cannot load backend 'TkAgg' which requires the 'tk' interactive framework, as 'headless' is currently running
 # matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 import numpy as np
 
-# noinspection PyUnresolvedReferences
-import pandas as pd
+
 from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
 from matplotlib.collections import LineCollection, PatchCollection
 from matplotlib.lines import Line2D
 from matplotlib.patches import Rectangle
 
+from omicron.core.talib import moving_average
+
+# Cannot load backend 'TkAgg' which requires the 'tk' interactive framework, as 'headless' is currently running
+# matplotlib.use("TkAgg")
+
 
 class Candlestick:
     def __init__(
         self,
-        row=6,
+        row=2,
         col=1,
         gridspec_kw=None,
-        fig_size=(8, 8),
+        fig_size=(12, 8),
         dpi=60,
         plot_window_size: int = 60,
-        lw=0.4,
-        bw=0.6,
         lfs=12,
         ma_lw=0.6,
     ):
@@ -37,15 +37,16 @@ class Candlestick:
         if gridspec_kw is None:
             gridspec_kw = {"height_ratios": [3, 1] * int(row / 2)}
         self.dpi = dpi
-        self.lw = lw
-        self.bw = bw
+        self.bw = (fig_size[0] * 4)/ plot_window_size
         self.ma_lw = ma_lw
         self.lfs = lfs
         self.fig, self.axes = plt.subplots(
             row, col, gridspec_kw=gridspec_kw, figsize=fig_size, sharex=True, dpi=dpi
         )
         # todo: parameterize
-        plt.subplots_adjust(hspace=0.02)
+        plt.subplots_adjust(hspace=0)
+        self.cm = {5: "b", 10: "g", 20: "c", 60: "m", 120: "y", 250: "tab:orange", "raw": "tab:gray"}
+
 
     def init_fig(self, y_lims=None):
         for i, ax in enumerate(self.axes):
@@ -67,9 +68,8 @@ class Candlestick:
 
         self.axes[-1].spines["bottom"].set_visible(False)
 
-        self.fig.tight_layout(h_pad=0.02)
-        # cls.fig.canvas.draw()
-        pass
+        self.fig.tight_layout(h_pad=0)
+        # self.fig.canvas.draw()
 
     def close(self):
         self.fig.close()
@@ -79,7 +79,7 @@ class Candlestick:
         bars: np.array,
         ma_groups,
         ax_candle_stick=None,
-        ax_turnover=None,
+        ax_volume=None,
         name: str = None,
     ):
         """
@@ -89,46 +89,61 @@ class Candlestick:
             bars: the dataframe which used to draw the candle stick
             ma_groups: hint for drawing which moving average line
             ax_candle_stick: the axis to draw the candlestick
-            ax_turnover: the axis to draw the volume n_bars
+            ax_volume: the axis to draw the volume n_bars
             name: for debug only
         return:
         """
-        # hide tick
-        ax_candle_stick = ax_candle_stick or self.axes[0]
-        ax_turnover = ax_turnover or self.axes[1]
+        n = self.plot_window_size
+        bars = bars.copy()
+        factor = np.max(bars["high"])
+        for key in ["open", "close", "high", "low"]:
+            bars[key] = bars[key] / factor
 
+        bars["volume"] /= np.nanmean(bars["volume"])
+
+        ax_candle_stick = ax_candle_stick or self.axes[0]
+        ax_volume = ax_volume or self.axes[1]
+
+        ax_candle_stick.xaxis.set_visible(False)
         # draw candlestick
-        facecolor = self.calc_facecolor(bars[-1 * self.plot_window_size :])
-        edgecolor = self.calc_edgecolor(bars[-1 * self.plot_window_size :])
+        # facecolor = self.calc_facecolor(bars[-n :])
+        facecolor = "w"
+        edgecolor = self.calc_edgecolor(bars[-n:])
 
         self.candle_stick_plot(
             ax_candle_stick,
-            bars[-1 * self.plot_window_size :],
+            bars[-n:],
             facecolor=facecolor,
             edgecolor=edgecolor,
         )
 
         # draw ma
         for i, win in enumerate(ma_groups or []):
-            ma = (
-                bars["close"]
-                .interpolate()
-                .rolling(win)
-                .mean()[-1 * self.plot_window_size :]
-            )
-            ma.index -= ma.index[0]
-            line = Line2D(ma.index, ma.values, color=f"C{i}", linewidth=self.ma_lw)
+            ma = moving_average(bars["close"], win)[-n:]
+            line = Line2D(range(len(ma)), ma, color=self.cm[win], linewidth=self.ma_lw)
             ax_candle_stick.add_line(line)
 
-        # draw turnover
-        ups = (
-            bars["close"][-self.plot_window_size :]
-            > bars["open"][-self.plot_window_size :]
+        # draw volume
+        ups = bars["close"][-n:] > bars["open"][-n:]
+
+        volume = bars["volume"][-n:]
+        frames = self.format_frames(bars["frame"][-n:])
+        ax_volume.bar(
+            range(n), volume, color=np.where(ups, "r", "g"), width=self.bw
         )
-        turnover = bars["turnover"][-self.plot_window_size :]
-        ax_turnover.bar(
-            range(len(turnover)), turnover, color=np.where(ups, "r", "g"), width=self.bw
-        )
+
+        positions = list(np.arange(n//8 + 1) * 8)
+        labels = [frames[i] for i in positions]
+        ax_volume.set_xticks(positions)
+        ax_volume.set_xticklabels(labels, rotation=45)
+
+    def format_frames(self, frames):
+        if frames[0].hour != 0:
+            fmt = "YY-MM-DD HH:mm"
+        else:
+            fmt = "YY-MM-DD"
+
+        return [arrow.get(frame).format(fmt) for frame in frames]
 
     def calc_edgecolor(self, bars: np.array) -> np.array:
         """
@@ -148,7 +163,7 @@ class Candlestick:
             bars: the bars data which used to draw the candle stick
         """
         # fixme: use real buy/sell limit function
-        pct = (bars["close"] + 0.01) / bars["close"].shift(1) - 1
+        pct = (bars["close"][1:] + 0.01) / bars["close"][:-1] - 1
 
         colors = np.array(["w"] * len(bars))
         colors[np.where(pct <= -0.099)] = "g"
@@ -166,35 +181,31 @@ class Candlestick:
             bars: normalized df
             w: the width of stem part
         """
-        vertex_top = np.zeros((len(bars), 2, 2))
-        vertex_bottom = np.zeros((len(bars), 2, 2))
-
         rects = []
-        for i in range(len(bars)):
-            o, c, l, h = (
-                bars.iloc[i]["open"],
-                bars.iloc[i]["close"],
-                bars.iloc[i]["low"],
-                bars.iloc[i]["high"],
-            )
-            if o >= c:
-                vertex_top[i] = [[i, o], [i, h]]
-                vertex_bottom[i] = [[i, l], [i, c]]
-            else:
-                vertex_top[i] = [[i, c], [i, h]]
-                vertex_bottom[i] = [[i, l], [i, o]]
-
-            rect = Rectangle(
-                (i - self.bw / 2.0, min(bars.iloc[i]["open"], bars.iloc[i]["close"])),
+        o, c, h, l = bars["open"], bars["close"], bars["high"], bars["low"]
+        rects = [
+            Rectangle(
+                (i - self.bw / 2.0, min(o[i], c[i])),
                 width=self.bw,
-                lw=self.lw,
-                height=abs(bars.iloc[i]["open"] - bars.iloc[i]["close"]),
+                lw=self.bw/4,
+                height=abs(o[i] - c[i]),
             )
-            rects.append(rect)
+            for i in range(len(bars))
+        ]
 
-        for vertex in [vertex_top, vertex_bottom]:
-            line_collection = LineCollection(vertex, color=edgecolor)
-            ax.add_collection(line_collection)
+        up_bottom = np.vstack((np.arange(len(bars)), np.where(o >= c, o, c), ))
+        up_top = np.vstack((np.arange(len(bars)), h))
+
+        up = zip(np.transpose(up_top), np.transpose(up_bottom))
+
+        down_top = np.vstack((np.arange(len(bars)), np.where(o >= c, c, o)))
+        down_bottom = np.vstack((np.arange(len(bars)), l))
+
+        down = zip(np.transpose(down_top), np.transpose(down_bottom))
+
+        ax.add_collection(LineCollection(up, colors=edgecolor, linewidths=self.bw/2))
+        ax.add_collection(LineCollection(down, colors=edgecolor, linewidths=self.bw/2))
 
         rect_pc = PatchCollection(rects, edgecolor=edgecolor, facecolor=facecolor)
         ax.add_collection(rect_pc)
+        ax.autoscale_view()
