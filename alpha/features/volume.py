90dd9ed83a6ae6711236607bcc4f2a65c63219f2
zillionare <code@jieyu.ai>
2021-11-21 15:23:36 +0800

    🦄 refactor: 成交量特征改为最大成交量量比方向加其后反向成交量特征
    返回最大成交量量比（与最小量比)及方向，其后反向成交量及与最大量之比

--- alpha/features/volume.py
+++ alpha/features/volume.py
@@ -3,36 +3,36 @@ import numpy as np
 from alpha.core.features import fillna, replace_zero, top_n_argpos
 
 
-def top_volume_direction(bars: np.array, n=8):
-    """计算`n`周期内，最大3次成交量的方向和量能变化的幅度
+def top_volume_direction(bars: np.array, n=21):
+    """计算`n`周期内，较大成交量的方向和比值。
 
     成交量方向：如果当前股价上涨则成交量方向为1，下跌则为-1
-
-    Note: 8 is one of fabnaci numbers
+    计算方法：
+        1. 找出最大成交量的位置
+        2. 找出其后一个最大异向成交量的位置
+        3. 总共返回一个2维数组，0表示不存在（不适用）
 
     args:
-        bars: 包含了OHLC和volumne的行情数据，类型为numpy structured array
+        bars: 包含了OHLC和volume的行情数据，类型为numpy structured array
         n: 参与计算的周期。太长则影响到最大成交量的影响力。
 
     """
-    bars_ = bars[-n:]
+    bars = bars[-n:]
+    volume = bars["volume"]
 
-    close = fillna(bars_["close"].copy())
-    open_ = fillna(bars_["open"].copy())
-    volume = bars_["volume"]
+    flags = np.select((bars["close"] >= bars["open"],), [1], -1)
 
-    avg = np.nanmean(volume)
-    volume = replace_zero(volume.copy())
+    vmin = np.min(volume)
+    pmax = np.argmax(volume)
 
-    # 涨跌，假定close > open,意味着方向为买进
-    flags = np.where((close > open_), 1, -1)
+    vr = [volume[pmax] / vmin, 0]
 
-    vr = volume / avg
-    indice = top_n_argpos(vr, 3)
+    vol = (volume * flags)[pmax:]
+    vmax = vol[0]
 
-    # 加上方向
-    vr = vr * flags
+    if flags[pmax] == 1 and np.any(vol[1:] < 0):
+        vr[1] = np.min(vol) / vmax
+    elif flags[pmax] == -1 and np.any(vol[1:] > 0):
+        vr[1] = np.max(vol) / vmax
 
-    # 按时间先后排列
-    indice = np.sort(indice)
-    return vr[indice]
+    return vr
